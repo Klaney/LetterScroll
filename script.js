19504eb4e56a798370fbb212a896da6aed057aa5
YOUR-USERNAME <YOUR-EMAIL-ADDRESS>
2015-10-09 16:31:32 -0700

    added my wordbank

---
+++ script.js
@@ -0,0 +1,5 @@
+var wordBank = ["smoke","bank","dinosaur","delicious","crackbaby","winner","loser","football","seachickens",
+"baseball","elmo","anything","scroll","amelia","jones","thermos","hike","yosemite","freecell","keyboard",
+"enter","exit","carpathia","teutonic","prussia","duchy","castile","habsburg","britain","washington",
+"samuel","clemens","enterprise","picard","captain","millenium","commonwealth","novgorod","muscovy","tokugawa",
+"samnite","epirote"];
