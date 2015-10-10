255565036d8bc1bb657a35943141962e39a1a77b
YOUR-USERNAME <YOUR-EMAIL-ADDRESS>
2015-10-09 17:00:33 -0700

    added a function, some variables and to add a word in the div, not sure if it works though

--- script.js
+++ script.js
@@ -3,3 +3,21 @@ var wordBank = ["smoke","bank","dinosaur","delicious","crackbaby","winner","lose
 "enter","exit","carpathia","teutonic","prussia","duchy","castile","habsburg","britain","washington",
 "samuel","clemens","enterprise","picard","captain","millenium","commonwealth","novgorod","muscovy","tokugawa",
 "samnite","epirote"];
+var lvl1 = 1500;
+var lvl2 = 1200;
+var lvl3 = 900;
+var lvl4 = 500;
+
+var startGame = function(){
+	setInterval(function (){
+		$("screen").text(wordBank[1]);
+	},1000);
+
+};
+
+$(document).ready("DOMcontentloaded", function(){
+	$("start").on("click", function(){
+		startGame();
+	});
+
+})
