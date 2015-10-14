9bd3cd1c3947670d5dd97155ca479204d594e539
YOUR-USERNAME <YOUR-EMAIL-ADDRESS>
2015-10-14 13:27:48 -0700

    just needs CSS

--- script.js
+++ script.js
@@ -11,10 +11,40 @@ var wordCount = 0;
 var gameRunning = false;
 var theInterval;
 var gameCheckInterval;
-var gameTurn = 0;
-var player1 = false;
-var player2 = false;
+var gameTurn = 1;
+var player1Score = 0;
+var player2Score = 0;
 
+var startGame = function(){
+	gameRunning = true;
+	whoseTurn();
+	continueGame();
+	//shuffle the bank 
+	shuffle(wordBank);
+	theInterval = setInterval(function (){
+		$(".screen").append("<p>"+ wordBank[index++ % wordBank.length] + "</p>");
+		wordBank.splice(index , 1);
+		console.log(wordBank.length);
+		console.log(playerTime);
+		console.log("test");
+		$(".screen p").animate({
+			left: '95%'}, 3000, function() {
+				if(gameRunning){
+				endGame();
+			};
+		});
+	},playerTime);
+};
+
+var whoseTurn = function(){
+	if (gameTurn % 2 === 1){
+		alert("Player 1 turn");
+	} else {
+		alert("Player 2 turn");
+	}
+}
+
+//The Fisher-Yates shuffle. Found the code here. http://bost.ocks.org/mike/shuffle/
 function shuffle(array) {
   var m = wordBank.length, t, i;
   // While there remain elements to shuffle…
@@ -29,50 +59,9 @@ function shuffle(array) {
   return array;
 }
 
-var checkEnd = function(){
-	// while (gameRunning){
-		var currentWord = $("p").first();
-		var wordPosition = currentWord.position();
-		// console.log(wordPosition);
-		// if (wordPosition.left >= 100){
-		// 	endGame();
-		// };
-	// };
-}
-
-var endGame = function(){
-	clearInterval(gameCheckInterval);
-	clearInterval(theInterval);
-		$(".screen").empty();
-		gameRunning = false;
-		wordCount = 0;
-		gameTurn++;
-		alert("You lost");
-}
-
-var startGame = function(){
-	gameRunning = true;
-	continueGame();
-	theInterval = setInterval(function (){
-		$(".screen").append("<p>"+ wordBank[index++ % wordBank.length] + "</p>");
-		wordBank.splice(index , 1);
-		console.log(wordBank.length);
-		console.log(playerTime);
-		$(".screen p").animate({
-			left: '95%'}, 3000, function() {
-
-				endGame();
-		});
-	},playerTime);
-	// setTimeout(checkEnd,playerTime);
-	gameCheckInterval = setInterval(checkEnd, 100);
-};
-
 var continueGame = function(){
 	console.log(wordCount);
-	//shuffle the bank 
-	shuffle(wordBank);
-	//put a new p element with a word into the box
+	//adjust time to be quicker the higher the score goes
 	if (gameRunning === true){
 		if (wordCount <= 3){
 			playerTime = 2000;
@@ -84,13 +73,56 @@ var continueGame = function(){
 			playerTime = 1000;
 		} else if (wordCount >= 16){
 			endGame();
-			gameTurn++;
 		};
 	};
 };
 
+var endGame = function(){
+	clearInterval(theInterval);
+	$(".screen").empty();
+	gameRunning = false;
+	if (wordCount === 16){
+		//determine which player to give score to
+		whichPlayerScore();
+
+		alert("You won!")
+		} else {
+		whichPlayerScore();
+		alert("You lost!")
+	};
+	wordCount = 0;
+	gameTurn++;
+}
+
+var whichPlayerScore = function(){
+	//Give player a player a score		
+	if (gameTurn % 2 === 1){
+		player1Score = player1Score + wordCount;
+		$("#p1score").html(player1Score);
+		return player1Score;
+	} else if (gameTurn % 2 === 0){
+		player2Score = player2Score + wordCount;
+		$("#p2score").html(player2Score);
+		whoWon();
+		return player2Score;
+	};
+	
+}
+
+var whoWon = function(){
+	//determine who won
+	if (player1Score > player2Score){
+		alert("Player 1 Wins!");
+	} else {
+		alert("Player 2 Wins!");
+	}
+};
+
 $(document).ready(function(){
+	//append player scores
+	
 	// Set enter to 'click' on keyup
+
 	$("#words").keyup(function(event){
     	if(event.keyCode == 13){
         	$("#submit").click();
@@ -101,11 +133,10 @@ $(document).ready(function(){
 		event.preventDefault();
 		startGame();
 		$("#words").focus();
-		/* stuff to do after animation is complete */
-		console.log("clicked");
 	});
 	//On submission of correct word, remove the current word.
 	$("#submit").on("click", function(){
+		console.log("click")
 		if (gameRunning === true){
 		 var theWord = $("p:nth-child(1)").text();
 		if ($("#words").val() === theWord){
@@ -118,6 +149,12 @@ $(document).ready(function(){
 		continueGame();
 		};
 	});
-	$(".screen").on("p", function(){
-	})
+	//Reset the game
+	$("#reset").on("click", function(){
+		gameTurn = 1;
+		player1Score = 0;
+		player2Score = 0;
+		$("#p1score").html(player1Score);
+		$("#p2score").html(player2Score);
+	});
 })
